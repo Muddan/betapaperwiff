a3f422fcee70cadab61240ecb51fed69e70560dd
Ubuntu <ubuntu@ip-172-31-24-211.ap-southeast-1.compute.internal>
2019-04-22 18:11:09 +0000

    removed nuxt folder

---
+++ frontend/vue.config.js
@@ -0,0 +1,9 @@
+// vue.config.js
+module.exports = {
+  // ...
+  devServer: {
+    host: 'ec2-52-221-226-186.ap-southeast-1.compute.amazonaws.com',
+    port: 80, // CHANGE YOUR PORT HERE!
+  },
+  // ...
+}
